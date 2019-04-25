dee94cae35dacfb7c4aa9a86f62fe9b9a94dad33
Antonio Arellano <antonio.941027@gmail.com>
2019-04-25 10:04:15 -0500

    Creacion del proyecto

---
+++ Carrito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carrito
+//
+//  Created by José Antonio Arellano Mendoza on 4/25/19.
+//  Copyright © 2019 José Antonio Arellano Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
